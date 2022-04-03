336d0a3a7ea939157c019c66a337366a091fdc1b
Shokhrookh <shokh7997@gmail.com>
2022-04-03 15:14:58 +0500

    Navlink

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import s from './Navbar.module.css'
 
 function Navbar() {
@@ -6,8 +7,8 @@ function Navbar() {
 	 
 		 
 			 <div className={s.nav}>
-				 <div className={s.item}><a href="/profile">Profile</a></div>
-				 <div className={s.item}><a href="/dialogs">Dialogs</a></div>
+				 <div className={s.item}><NavLink to="/profile">Profile</NavLink></div>
+				 <div className={s.item}><NavLink to={"/dialogs"}>Dialogs</NavLink></div>
 				 <div className={s.item}>Communities</div>
 				 <div className={s.item}>Friends</div>
 			 </div>
