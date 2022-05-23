9c798747425daf04918fca0d1a49cf6dfe291863
Shokhrookh <shokh7997@gmail.com>
2022-05-23 21:03:51 +0500

    removed rerender.js, замыкания

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import state, { addPost, updatePostText } from './components/State/state';
 import { listener } from './components/State/state';
 
